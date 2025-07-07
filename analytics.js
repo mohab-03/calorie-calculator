07c3924ecf0c17f5b6c3a8540e279cb7de1f8894
mohab-03 <mohab4878@gmail.com>
2025-07-07 19:11:32 +0300

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -1,5 +1,9 @@
-// analytics.js
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-MFK8KTL6SS');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MFK8KTL6SS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MFK8KTL6SS');
+</script>
